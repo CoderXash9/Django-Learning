f5a2494eef761695d40f9d42055aec79e77b0824
CoderXash9 <asahwiniprohit@gmail.com>
2026-07-04 00:12:26 +0530

    imported Member model to send it to the template

--- 01_First_Project/first_project/members/views.py
+++ 01_First_Project/first_project/members/views.py
@@ -1,7 +1,13 @@
 from django.http import HttpResponse
 from django.template import loader
+from .models import Member
 
 
 def members(request):
-    template = loader.get_template("myfirst.html")
-    return HttpResponse(template.render())
+    mymebers = Member.objects.all().values()
+    template = loader.get_template("allmembers.html")
+    context = {
+        "mymembers": mymebers,
+    }
+
+    return HttpResponse(template.render(context, request))
