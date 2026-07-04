b7c800de54ea5437e91d0e877fdc1916a0a3e57b
CoderXash9 <asahwiniprohit@gmail.com>
2026-07-04 15:37:21 +0530

    changed the debug property to FALSE , to allow the project to run from the local host

--- 01_First_Project/first_project/first_project/settings.py
+++ 01_First_Project/first_project/first_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-%v^&_!ll$s@*_sw)(zxp_v)yjy#!)n7x94mx!l0a)=$^-wq=r1"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
