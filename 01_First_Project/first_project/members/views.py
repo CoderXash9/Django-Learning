e4dff1644340e2ef0c54715a28eb0c73399f4031
CoderXash9 <asahwiniprohit@gmail.com>
2026-07-07 04:52:05 +0530

    created another view of name testing for importing data from Member model

--- 01_First_Project/first_project/members/views.py
+++ 01_First_Project/first_project/members/views.py
@@ -45,3 +45,17 @@ def testing(request):
         "firstname": "Linus",
     }
     return HttpResponse(template.render(context, request))
+
+
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
+from .models import Member
+
+
+def testing(request):
+    mymembers = Member.objects.all().values()
+    template = loader.get_template("template.html")
+    context = {
+        "mymembers": mymembers,
+    }
+    return HttpResponse(template.render(context, request))
