eb533802a100e4888900f9f00cc344055e619db4
CoderXash9 <asahwiniprohit@gmail.com>
2026-07-13 23:02:13 +0530

    changing admin

--- 01_First_Project/first_project/members/admin.py
+++ 01_First_Project/first_project/members/admin.py
@@ -2,10 +2,13 @@ from django.contrib import admin
 from .models import Member
 
 
-@admin.register(Member)
 class MemberAdmin(admin.ModelAdmin):
     list_display = (
         "firstname",
         "lastname",
         "joined_date",
     )
+    prepopulated_fields = {"slug": ("firstname", "lastname")}
+
+
+admin.site.register(Member, MemberAdmin)
