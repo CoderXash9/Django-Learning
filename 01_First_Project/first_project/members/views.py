0e35d83b7c6274d69d30ea86d64dcada11b534d6
CoderXash9 <asahwiniprohit@gmail.com>
2026-07-13 23:16:08 +0530

    modified view to handle incoming requests of slug instead of id

--- 01_First_Project/first_project/members/views.py
+++ 01_First_Project/first_project/members/views.py
@@ -8,22 +8,34 @@ def main(request):
 
 def members(request):
     mymembers = Member.objects.all()
-    return render(request, "allmembers.html", {
-        "mymembers": mymembers,
-    })
+    return render(
+        request,
+        "allmembers.html",
+        {
+            "mymembers": mymembers,
+        },
+    )
 
 
-def details(request, id):
-    mymember = Member.objects.get(id=id)
-    return render(request, "details.html", {
-        "mymember": mymember,
-    })
+def details(request, slug):
+    mymember = Member.objects.get(slug=slug)
+    return render(
+        request,
+        "details.html",
+        {
+            "mymember": mymember,
+        },
+    )
 
 
 def testing10(request):
     mymembers = Member.objects.all().order_by("lastname", "-id")
-    return render(request, "template.html", {
-        "mymembers": mymembers,
-        "fruits": ["Apple", "Banana", "Mango"],
-        "greeting": 1,
-    })
+    return render(
+        request,
+        "template.html",
+        {
+            "mymembers": mymembers,
+            "fruits": ["Apple", "Banana", "Mango"],
+            "greeting": 1,
+        },
+    )
